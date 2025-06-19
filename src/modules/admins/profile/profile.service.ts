073c76fe8537e87765fb085577220c3a105187a0
Hung Nguyen <nguyenmanhhung071203@gmail.com>
2025-06-19 21:29:35 +0700

    ajdh

--- src/modules/admins/profile/profile.service.ts
+++ src/modules/admins/profile/profile.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  async verifyCaptcha(token: string, ip: string) {
+    const response = await axios.post(process.env.CAPTCHA_URL, {
+      secret: process.env.CAPTCHA_SECRET_KEY,
+      response: token,
+      remoteip: ip,
+    });
+    if (!response.data.success)
+      throw new Error(
+        'Captcha không hợp lệ, vui lòng đợi vài giây trước khi thực hiện hành động!',
+      );
+  }
+}
