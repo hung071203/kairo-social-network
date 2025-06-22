3b0fb9514dd2960f99457c41d6ab1ff262230279
Hung Nguyen <nguyenmanhhung071203@gmail.com>
2025-06-22 12:49:52 +0700

    ajdh

--- src/modules/admins/noti-management/noti-management.controller.ts
+++ src/modules/admins/noti-management/noti-management.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Render, UseFilters, UseGuards } from '@nestjs/common';
 import { NotiManagementService } from './noti-management.service';
+import { ModeratorsAuthGuard } from 'src/common/guards/jwt/jwt.guard';
+import { WebExceptionFilter } from 'src/common/filters';
 
 @Controller('noti-management')
+@UseGuards(ModeratorsAuthGuard)
 export class NotiManagementController {
   constructor(private readonly notiManagementService: NotiManagementService) {}
+
+  @Get()
+  @Render('admins/pages/notification/index.njk')
+  @UseFilters(WebExceptionFilter)
+  async getNotiManagement() {
+    try {
+      return {
+        title: 'Quản lý thông báo',
+        content: 'admins/pages/notification/index.njk',
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 }
